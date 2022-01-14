68004efc98d192e6a4cfa01b3f8e601dbd694956
Beatriz Dias <biiadiassouza@gmail.com>
2022-01-14 12:14:41 -0300

    Index Correção

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -2,17 +2,17 @@ import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
-
     return (
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSmovie</h1>
-                    <a href="https://github.com/beatrizds"></a>
-                        <div className="dsmovie-contact-container">
+                    <a href="https://github.com/beatrizds">
+                        <div className='dsmovie-contact-container'>
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
-                    </div>
+                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                        </div>
+                    </a>
                 </div>
             </nav>
         </header>
